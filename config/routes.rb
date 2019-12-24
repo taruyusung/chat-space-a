1998e9c0f453fbe7a7aa34f4937f3165f6341548
taruyusung <58249888+taruyusung@users.noreply.github.com>
2019-12-24 20:35:03 +0900

    Revert "k"
    This reverts commit f491026913f5174d10e15789251751ad7f9274ac.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  root to: 'messages#index'
+ root to: 'messages#_main_chat'
+ root to: 'messages#_side_bar'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
